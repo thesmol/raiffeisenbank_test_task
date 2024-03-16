87366b76f906c2855dd48d7385f54cb4910280f3
Медведев Иван <ranzar2000@gmail.com>
2024-03-16 19:30:26 +0300

    Make the UsersTable component universal
     The component now accepts displayable fields as props and handles complex types such as "address" and "company", converting them to displayable strings.

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -1,53 +1,62 @@
 import { FC } from "react";
 import { CustomTable } from "vienna-ui";
-import { IUser } from '../types/types';
+import { IAddress, ICompany, IUser } from '../types/types';
 
 interface UserTableProps {
-    title: string;
     users: IUser[];
+    headers?: (keyof IUser)[];
     onUserClick?: () => void;
 }
 
-const UsersTable: FC<UserTableProps> = ({ users, onUserClick }) => {
+/**
+ * Обрабатывает сложные типы, такие как IAddress и ICompany, преобразуя их в строку.
+ *
+ * @param fieldValue - Значение поля, которое может быть примитивом или объектом.
+ * @returns Возвращает строку, если fieldValue является объектом типа IAddress или ICompany. 
+ * В противном случае возвращает исходное значение поля.
+ */
+const handleComplexType = (fieldValue: unknown) => {
+    if (typeof fieldValue === 'object' && fieldValue !== null) {
+        if ('street' in fieldValue
+            && 'suite' in fieldValue
+            && 'city' in fieldValue
+            && 'zipcode' in fieldValue
+        ) {
+            const address = fieldValue as IAddress;
+            return `${address.city}, ${address.street}, ${address.suite}, ${address.zipcode}`;
+        }
+        else if ('name' in fieldValue && 'catchPhrase' in fieldValue) {
+            const company = fieldValue as ICompany;
+            return `${company.name}, ${company.catchPhrase}`;
+        }
+    }
+    return fieldValue;
+}
+
+const UsersTable: FC<UserTableProps> = ({
+    users,
+    headers = ['id', 'name', 'email', 'address'],
+    onUserClick
+}) => {
     return (
         <CustomTable>
             <CustomTable.Head>
                 <CustomTable.Row>
-                    <CustomTable.Header>
-                        №
-                    </CustomTable.Header>
-                    <CustomTable.Header>
-                        Имя
-                    </CustomTable.Header>
-                    <CustomTable.Header>
-                        Никнейм
-                    </CustomTable.Header>
-                    <CustomTable.Header>
-                        Email
-                    </CustomTable.Header>
-                    <CustomTable.Header>
-                        Адрес
-                    </CustomTable.Header>
+                    {headers.map((header, index) => (
+                        <CustomTable.Header key={index}>
+                            {header}
+                        </CustomTable.Header>
+                    ))}
                 </CustomTable.Row>
             </CustomTable.Head>
             <CustomTable.Body>
                 {users.map(user => (
                     <CustomTable.Row key={user.id} onClick={onUserClick}>
-                        <CustomTable.Data>
-                            {user.id}
-                        </CustomTable.Data>
-                        <CustomTable.Data>
-                            {user.name}
-                        </CustomTable.Data>
-                        <CustomTable.Data>
-                            {user.username}
-                        </CustomTable.Data>
-                        <CustomTable.Data>
-                            {user.email}
-                        </CustomTable.Data>
-                        <CustomTable.Data>
-                            {`${user.address.city}, ${user.address.street}, ${user.address.suite}`}
-                        </CustomTable.Data>
+                        {headers.map((header, index) => (
+                            <CustomTable.Data key={index}>
+                                {handleComplexType(user[header]) as React.ReactNode}
+                            </CustomTable.Data>
+                        ))}
                     </CustomTable.Row>
                 ))}
             </CustomTable.Body>
