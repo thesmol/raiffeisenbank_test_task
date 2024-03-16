eef18463c2fd3a221e6f7d580fc2a64e84a04aaa
Медведев Иван <ranzar2000@gmail.com>
2024-03-16 17:18:24 +0300

    Add UserService class with getAll and getById methods
    The getAll method retrieves a paginated list of users, and the getById method retrieves a single user by their ID. Both methods return a Promise that resolves to an AxiosResponse containing an array of IUser objects or a single IUser object, respectively.

---
+++ src/API/UserService.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosResponse } from "axios";
+import { IUser } from "../types/types";
+
+/**
+ * Сервис для работы с пользователями.
+ */
+export default class UserService {
+    /**
+     * Получить всех пользователей с пагинацией.
+     *
+     * @param limit - Количество пользователей на странице (по умолчанию 10).
+     * @param page - Номер страницы (по умолчанию 1).
+     * @returns Промис с ответом от сервера, содержащим массив пользователей.
+     */
+    static async getAll(limit: number = 10, page: number = 1): Promise<AxiosResponse<IUser[]>> {
+        const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users', {
+            params: {
+                _limit: limit,
+                _page: page
+            }
+        });
+
+        return response;
+    }
+
+    /**
+     * Получить пользователя по идентификатору.
+     *
+     * @param Идентификатор пользователя.
+     * @returns Промис с ответом от сервера, содержащим информацию о пользователе.
+     */
+    static async getById(id: number): Promise<AxiosResponse<IUser>> {
+        const response = await axios.get<IUser>('https://jsonplaceholder.typicode.com/users/' + id);
+
+        return response;
+    }
+}
